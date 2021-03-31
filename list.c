2c6ee934ec80d21fd6cdccae9a7323d59a424a64
Replit user <replituser@example.com>
2021-03-31 09:26:05 +0000

    new try

--- list.c
+++ list.c
@@ -119,8 +119,9 @@ void * popBack(List * list) {
 }
 
 void * popCurrent(List * list) {
+    Node* aux=list->current->prev;
     void* dato = (void*) list->current->data;
-    list->current->prev->next=list->current->next;
+    aux->next=list->current->next;
     free (list->current);
     
     return dato;
